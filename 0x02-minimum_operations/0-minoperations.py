cf97d4e63883727795e4f2f2216e4ff81b095b4d
Jonathan Saina <51122713+Sainawj@users.noreply.github.com>
2024-10-17 12:51:54 +0300

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -12,36 +12,41 @@ operations needed, or 0 if n is impossible to achieve.
 
 def minOperations(n):
     """
-    Calculate the minimum number of operations to reach exactly n characters of 'H'.
+    Calculate the minimum number of operations to reach exactly n
+    characters of 'H'.
 
     Args:
         n (int): The target number of 'H' characters.
 
     Returns:
-        int: The minimum number of operations needed to achieve n 'H' characters,
-             or 0 if it is impossible.
+        int: The minimum number of operations needed to achieve n
+             'H' characters, or 0 if it is impossible.
     """
     if n <= 1:
-        return 0  # If n is less than or equal to 1, it's impossible to achieve
+        return 0  # If n <= 1, it's impossible to achieve
 
     operations = 0  # Initialize the operation count
     divisor = 2  # Start checking for divisors from 2
 
     while n > 1:  # While n is greater than 1
-        while n % divisor == 0:  # While n is divisible by the current divisor
-            operations += divisor  # Add the divisor to the operations count
+        while n % divisor == 0:  # While n is divisible by divisor
+            operations += divisor  # Add divisor to operations count
             n //= divisor  # Reduce n by the divisor
-        divisor += 1  # Increment the divisor to check the next integer
+        divisor += 1  # Increment divisor to check the next integer
 
-    return operations  # Return the total number of operations
+    return operations  # Return total number of operations
+
 
 # Example test cases
 if __name__ == "__main__":
     n = 4
-    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+    print("Min # of operations to reach {} char: {}".format(n,
+          minOperations(n)))
 
     n = 12
-    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+    print("Min # of operations to reach {} char: {}".format(n,
+          minOperations(n)))
 
     n = 9
-    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+    print("Min # of operations to reach {} char: {}".format(n,
+          minOperations(n)))
